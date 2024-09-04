61292d5476d5c5ee8733a7e78e5437970fe4dcea
gk6294 <bartonchik05@gmail.com>
2024-09-04 20:45:42 +0800

    start2

--- 09-article-preview-component-master/assets/js/main.js
+++ 09-article-preview-component-master/assets/js/main.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const shareButton = document.querySelector('.js-toggle-tip');
-const shareTooltip = document.getElementById('share');
+const shareTooltip = document.querySelector('.js-tooltip');
 
 shareButton.addEventListener('click', () => {
     const isExpanded = shareButton.getAttribute('aria-expanded') === 'true';
     shareButton.setAttribute('aria-expanded', !isExpanded);
     shareTooltip.classList.toggle('invisible', isExpanded);
     shareTooltip.classList.toggle('opacity-0', isExpanded);
-    console.log(shareButton, shareTooltip)
   });
 
 });
