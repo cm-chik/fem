f8dd2d66f85cf4c79e963c19084362326e935598
barton05 <bartonchik05@gmail.com>
2024-09-06 12:19:01 +0800

    update

--- 09-article-preview-component-master/assets/js/main.js
+++ 09-article-preview-component-master/assets/js/main.js
@@ -5,4 +5,13 @@ function toogleTooltip () {
     const shareTooltip = document.getElementById('js-tooltip');
     shareTooltip.classList.toggle("opacity-0")
     }
+function hideTooltip() {
+  const shareTooltip = document.getElementById("js-tooltip");
+  shareTooltip.classList.add("opacity-0");
+}
 
+document.addEventListener("click", (event) => {
+  if (!event.target.matches(".js-toggle-tip *")) {
+    hideTooltip();
+  }
+});
