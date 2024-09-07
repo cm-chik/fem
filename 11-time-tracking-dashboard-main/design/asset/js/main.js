701d3b9e06be0e7631a96e00a7a98b8daef444f1
gk6294 <bartonchik05@gmail.com>
2024-09-08 02:21:36 +0800

    update

--- 11-time-tracking-dashboard-main/design/asset/js/main.js
+++ 11-time-tracking-dashboard-main/design/asset/js/main.js
@@ -1,1 +1,8 @@
-document.getElementById("title") 
+const buttons = document.querySelectorAll(".frequencySelect");
+
+buttons.forEach((button) => {
+  button.dataset.option; //?
+  button.addEventListener("click", () => {
+    //console.log(button.dataset.option)}
+  });
+});
