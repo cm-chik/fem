ed1f2440b9425e3db07004ae4f53c0f689109fec
gk6294 <bartonchik05@gmail.com>
2024-09-08 21:30:41 +0800

    fix

--- 11-time-tracking-dashboard-main/assets/js/main.js
+++ 11-time-tracking-dashboard-main/assets/js/main.js
@@ -1,4 +1,5 @@
-const data = [];
+const cards = document.querySelectorAll(".card");
+
 const buttons = document.querySelectorAll(".frequencySelector");
 
 //TODO: can it be done by toggle?
@@ -9,30 +10,56 @@ const activatedCardOption = (button) => {
   button.classList.add("active");
 };
 
-const renderCard = (option) => {
-  data.forEach((activity) => {
-    const name = activity.title;
-    const activityClass = name.toLowerCase().replace(" ", "-");
-    const timeframeData = activity.timeframes[option];
-    const previousTimeFrame = getTimeFrameName(option);
-    console.log(name);
-  });
-};
-const calTimeFrame = (option) => {
+function renderCards(option) {
+  const cards = document.querySelectorAll('div[class^="card"]');
+  const timeframeName = getTimeFrameName(option);
+  const activityData = fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((activity) => {
+        activityTitle = activity.title.toLowerCase().replace(" ", "-");
+        cards.forEach((card) => {
+          if (card.classList.contains(activityTitle + "-card")) {
+            console.log(card);
+          }
+        });
+      });
+    });
+
+  activityData.then(
+    () => console.log(activityData)
+    // activityData.forEach((actData) => {
+    //   console.log(actData);
+    //   title = actData[title];
+    //cards.forEach((card, index) => {
+    //const activity =
+    // if (card.classList.contains(activity+'-card')){
+    // // Use querySelectorAll on the individual card
+    // thisTimeFrameData.forEach((dataElement) => {
+    //   const timeframeWord = dataElement.textContent; // Get the text content
+    //   // Assuming you want to update the text content of each dataElement
+    //   dataElement.textContent = timeframeWord; // Update or log as needed
+    // } else if () { }
+    //});
+    //});
+    //})
+  );
+}
+const getTimeFrameName = (option) => {
   if (option === "daily") {
-    return "yesterday";
+    return "Yesterday";
   } else if (option === "weekly") {
-    return "last week";
+    return "Last week";
   } else if (option === "monthly") {
-    return "last month";
+    return "Last month";
   }
 };
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    console.log(button.dataset.option);
     const ClickedOption = button.dataset.option;
-    activatedCardOption(button);
-    renderCard(ClickedOption);
+    renderCards(ClickedOption);
   });
 });
+
+renderCards("daily");
