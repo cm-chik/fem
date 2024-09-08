c6ee7d9e274dc9f14a85da4ecd3944230fc2d1c7
barton05 <bartonchik05@gmail.com>
2024-09-08 17:42:16 +0800

    activatedCardOption

--- 11-time-tracking-dashboard-main/assets/js/main.js
+++ 11-time-tracking-dashboard-main/assets/js/main.js
@@ -1,7 +1,19 @@
 const buttons = document.querySelectorAll(".frequencySelector");
 
+//TODO: can it be done by toggle?
+const activatedCardOption = (button) => {
+  buttons.forEach((button) => {
+    button.classList.remove;
+  });
+  button.classList.add("active");
+};
+const renderCard = () => {};
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    console.log(button.dataset);
+    console.log(button.dataset.option);
+    const ClickedOption = button.dataset.option;
+    activatedCardOption(button);
+    renderCard(ClickedOption);
   });
 });
