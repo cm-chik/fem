6654269ae07b91bd08fff50202b715c56e95b701
gk6294 <bartonchik05@gmail.com>
2024-09-10 09:22:52 +0800

    first js

--- 12-tip-calculator-app-main/assets/js/main.js
+++ 12-tip-calculator-app-main/assets/js/main.js
@@ -1,2 +1,16 @@
 const billAmount = document.querySelector(".billAmount");
 const peopleCount = document.querySelector(".peopleCount");
+const tipButtons = document.querySelectorAll(".tipsButton");
+const customTipPercentage = document.querySelector(".customTipPercentage");
+const tipPricePerPerson = document.querySelector(".tipPricePerPerson");
+const totalPricePerPerson = document.querySelector(".totalPricePerPerson");
+const resetButton = document.querySelector(".resetButton");
+
+tipButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    tipPricePerPerson.textContent =
+      (button.value * billAmount.value) / peopleCount;
+    totalPricePerPerson.textContent =
+      (button.value * (1 + billAmount.value)) / peopleCount;
+  });
+});
