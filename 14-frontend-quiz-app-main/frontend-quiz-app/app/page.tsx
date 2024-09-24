3dc429c5a58cf9028e6ba89c19d86fa944f45119
gk6294 <bartonchik05@gmail.com>
2024-09-25 05:53:24 +0800

    dark mode button

--- 14-frontend-quiz-app-main/frontend-quiz-app/app/page.tsx
+++ 14-frontend-quiz-app-main/frontend-quiz-app/app/page.tsx
@@ -1,9 +1,30 @@
-import Image from "next/image";
-
+"use client";
+import { useEffect } from "react";
 export default function Home() {
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  };
   return (
-    <div className="">
-      <main className=""></main>
+    <div>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <div className="bg-white dark:bg-black">
+        Welcome to the <b>Frontend Quiz!</b>
+        <br />
+        Pick a subject to get started.
+      </div>
     </div>
   );
 }
