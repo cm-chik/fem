b7f9ea62b979637e2352656695479f0fea964f88
gk6294 <bartonchik05@gmail.com>
2024-09-26 15:25:50 +0800

    css completed

--- 15-interactive-rating-component-main/interactive-rating-component/app/components/feedback.tsx
+++ 15-interactive-rating-component-main/interactive-rating-component/app/components/feedback.tsx
@@ -1,7 +1,17 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { useState } from "react";
 
 const FeedbackForm = () => {
+  const [rating, setRating] = useState(0);
+  const [isSelected, setIsSelected] = useState(false);
+
+  const selectRating = (rating: number) => {
+    setRating(rating);
+    setIsSelected(true);
+  };
+
   return (
     <div className="bg-gray-800 p-8 rounded-lg shadow-md flex flex-col space-y-6 max-w-[400px]">
       <Image
@@ -17,23 +27,24 @@ const FeedbackForm = () => {
         appreciated to help us improve our offering!
       </p>
       <div className="flex justify-between px-4 py-2">
-        <button className="p-1 bg-gray-700 rounded-full text-gray-400 w-10 h-10 text-xs active:bg-white active:text-gray-800 hover:bg-orange-500 hover:text-gray-800">
-          1
-        </button>
-        <button className="p-1 bg-gray-700 rounded-full text-gray-400 w-10 h-10 text-xs active:bg-white active:text-gray-800 hover:bg-orange-500 hover:text-gray-800">
-          2
-        </button>
-        <button className="p-1 bg-gray-700 rounded-full text-gray-400 w-10 h-10 text-xs active:bg-white active:text-gray-800 hover:bg-orange-500 hover:text-gray-800">
-          3
-        </button>
-        <button className="p-1 bg-gray-700 rounded-full text-gray-400 w-10 h-10 text-xs active:bg-white active:text-gray-800 hover:bg-orange-500 hover:text-gray-800">
-          4
-        </button>
-        <button className="p-1 bg-gray-700 rounded-full text-gray-400 w-10 h-10 text-xs active:bg-white active:text-gray-800 hover:bg-orange-500 hover:text-gray-800">
-          5
-        </button>
+        {[1, 2, 3, 4, 5].map((i) => (
+          <button
+            className={`p-1 bg-gray-700 rounded-full text-gray-400 w-10 h-10
+            text-xs  ${
+              rating === i ? "bg-white" : "bg-gray-800"
+            } active:text-gray-800 hover:bg-orange-500
+            hover:text-gray-800`}
+            value={i}
+            onClick={() => selectRating(i)}
+          >
+            {i}
+          </button>
+        ))}
       </div>
-      <button className="bg-orange-500 p-2 rounded-2xl text-gray-800 uppercase hover:bg-white">
+      <button
+        className="bg-orange-500 p-2 rounded-2xl text-gray-800 uppercase hover:bg-white disabled:bg-gray-500"
+        disabled={!isSelected}
+      >
         Submit
       </button>
     </div>
