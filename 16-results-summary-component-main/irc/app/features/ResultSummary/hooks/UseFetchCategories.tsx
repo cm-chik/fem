aa491482d49e2da8d27e406583ed8f0fffd6e3e7
gk6294 <bartonchik05@gmail.com>
2024-09-29 12:37:44 +0800

    changing source to deploy

--- 16-results-summary-component-main/irc/app/features/ResultSummary/hooks/UseFetchCategories.tsx
+++ 16-results-summary-component-main/irc/app/features/ResultSummary/hooks/UseFetchCategories.tsx
@@ -3,8 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 function useFetchCategories() {
   const { data, isLoading, error } = useQuery({
     queryKey: ["categories"],
-    queryFn: () =>
-      fetch("http://localhost:3000/api/").then((res) => res.json()),
+    queryFn: () => fetch("/data.json").then((res) => res.json()),
   });
   return { data, isLoading, error };
 }
