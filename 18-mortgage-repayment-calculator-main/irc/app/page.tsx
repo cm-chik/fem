0543738dabcf1b239defe55c0cac80e3200117b8
gk6294 <bartonchik05@gmail.com>
2024-10-02 01:51:08 +0800

    18-foundation

---
+++ 18-mortgage-repayment-calculator-main/irc/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        Mortgage Calculator Clear All Mortgage Amount Mortgage Term Interest
+        Rate Mortgage Type Repayment Interest Only Calculate Repayments Results
+        shown here Complete the form and click “calculate repayments” to see
+        what your monthly repayments would be. Your results Your results are
+        shown below based on the information you provided. To adjust the
+        results, edit the form and click “calculate repayments” again. Your
+        monthly repayments Total you&apos;ll repay over the term
+      </main>
+    </div>
+  );
+}
