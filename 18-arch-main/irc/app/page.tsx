371b73b3b6f0f548b4fb32256e02bd978533c0fe
gk6294 <bartonchik05@gmail.com>
2024-10-02 15:29:27 +0800

    update 18 to arch studio

---
+++ 18-arch-main/irc/app/page.tsx
@@ -0,0 +1,9 @@
+import "@/app/globals.css";
+import Image from "next/image";
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center h-screen min-h-screen p-4 text-plus-jakarta-sans bg-[#e3f4fc]">
+      <div className="m-3 grid max-w-[1200px] sm:flex sm:justify-between gap-8 items-center w-full p-6 bg-white rounded-xl"></div>
+    </main>
+  );
+}
