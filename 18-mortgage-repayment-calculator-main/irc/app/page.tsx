58f9467c5daa854fb9ff8543571ac6fd713fa141
gk6294 <bartonchik05@gmail.com>
2024-10-02 09:58:27 +0800

    layout

--- 18-mortgage-repayment-calculator-main/irc/app/page.tsx
+++ 18-mortgage-repayment-calculator-main/irc/app/page.tsx
@@ -1,15 +1,41 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Mortgage Calculator Clear All Mortgage Amount Mortgage Term Interest
-        Rate Mortgage Type Repayment Interest Only Calculate Repayments Results
-        shown here Complete the form and click “calculate repayments” to see
-        what your monthly repayments would be. Your results Your results are
-        shown below based on the information you provided. To adjust the
-        results, edit the form and click “calculate repayments” again. Your
-        monthly repayments Total you&apos;ll repay over the term
-      </main>
-    </div>
+    <main className="flex justify-center items-center sm:h-screen">
+      <div className="sm:flex gap-8">
+        <div className="flex flex-col gap-4 ">
+          <div className="flex justify-between">
+            <div>Mortgage Calculator</div>
+            <div>Clear All</div>
+          </div>
+
+          <label>Mortgage Amount</label>
+
+          <label>Mortgage Term</label>
+
+          <label>Interest Rate</label>
+
+          <label>Mortgage Type</label>
+
+          <label>Repayment</label>
+          <label>Interest Only</label>
+        </div>
+        <div>
+          <div>
+            Results shown here Complete the form and click “calculate
+            repayments” to see what your monthly repayments would be. Your
+            results Your results are shown below based on the information you
+            provided.
+          </div>
+          <div>
+            Repayment Interest Only Calculate Repayments Results shown here
+            Complete the form and click “calculate repayments” to see what your
+            monthly repayments would be. Your results Your results are shown
+            below based on the information you provided. To adjust the results,
+            edit the form and click “calculate repayments” again. Your monthly
+            repayments Total you&apos;ll repay over the term
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
